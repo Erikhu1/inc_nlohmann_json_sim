75ee8c36afcaccbcbe9e8206983fd609c7750327
erikhu1 <erik.hu@d-fine.com>
2026-01-12 17:40:56 +0000

    test fix 2

--- .devcontainer/S-CORE/post_create_script.sh
+++ .devcontainer/S-CORE/post_create_script.sh
@@ -5,4 +5,5 @@ source .venv/bin/activate
 
 # Install trustable
 pip install --require-hashes -r .devcontainer/S-CORE/requirements.txt
-pip install git+https://gitlab.com/CodethinkLabs/trustable/trustable@v2025.10.22
+pip install git+https://gitlab.com/CodethinkLabs/trustable/trustable@v2025.10.22 \
+  --hash=sha256:b97414c78f7dd19039fa47bc3f7645d5ecad77eb6da68ef0032b990dfea3c1e6
