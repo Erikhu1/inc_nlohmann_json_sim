0cd9e750cbf8951cb0f73e0928e381cfb3665d0d
erikhu1 <erik.hu@d-fine.com>
2026-01-13 09:48:52 +0000

    test no setuptools

--- .devcontainer/S-CORE/post_create_script.sh
+++ .devcontainer/S-CORE/post_create_script.sh
@@ -4,5 +4,5 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 # Install trustable
-pip install --require-hashes -r .devcontainer/S-CORE/requirements.txt
+pip install --require-hashes -r requirements.txt
 pip install git+https://gitlab.com/CodethinkLabs/trustable/trustable@83b4023d7e2bd2b984db2c81543266ce09a7cbf7
